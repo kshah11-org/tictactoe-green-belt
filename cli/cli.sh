08f091e95875432ff47dce3533c312a98a35ea2b
Kush Shah <kush.shah@ibm.com>
2023-12-22 14:16:19 +0000

    feat: add git actions with sf

--- cli/cli.sh
+++ cli/cli.sh
@@ -1,1 +1,1 @@
-echo -e "$(sf apex run -o test-mtrojax4yife@example.com -f cli/cli.apex | grep 'USER_DEBUG' | grep -Eo '".*"' | sed 's/&#124;/|/g')"
+echo -e "$(sf apex run -o test-hjawoxoxbzut@example.com -f cli/cli.apex | grep 'USER_DEBUG' | grep -Eo '".*"' | sed 's/&#124;/|/g')"
--- tests/ticTacToe.e2e.test.js
+++ tests/ticTacToe.e2e.test.js
@@ -3,20 +3,19 @@ const request = require("supertest");
 const bearerToken = process.env.TOKEN;
 const url = process.env.URL;
 
-describe('TicTacToe e2e test', () => {
-    describe('Given the /TicTacToe/newGame', () => {
-        test('return the game played in BOT mode to the user', () => {
-            request(url)
-            .get(`/services/api/TicTacToe/newGame`)
-            .set({
-              Authorization: `Bearer ${bearerToken}`
-            })
-            .expect(200)
-            .then(function (response) {
-              expect(response.body).toContain("Game Board Creation");
-              expect(response.body).toContain("Player O won the game! ");
-            });
-          });
-        
+describe("TicTacToe e2e test", () => {
+  describe("Given the /TicTacToe/newGame", () => {
+    test("return the game played in BOT mode to the user", async () => {
+      await request(url)
+        .get(`/services/api/TicTacToe/newGame`)
+        .set({
+          Authorization: `Bearer ${bearerToken}`
+        })
+        .expect(200)
+        .then(function (response) {
+          expect(response.body).toContain("Game Board Creation");
+          expect(response.body).toContain("Player O won the game! ");
+        });
     });
+  });
 });
